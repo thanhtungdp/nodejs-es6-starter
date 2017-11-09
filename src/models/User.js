1503ae0537251b789d580759e6994611325b6a97
Phan Thanh Tung <tungptkh@gmail.com>
2017-11-09 17:53:22 +0700

    update role

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose'
 
-const User = new mongoose.Schema(
-  {
-    email: String,
-    password: String,
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
-  }
-)
+const User = new mongoose.Schema({
+  email: String,
+  role: { type: String, default: 'user' },
+  password: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+})
 
 export default mongoose.model('User', User)
