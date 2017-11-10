e73163d4dd927740bf4cb22676c21d992683b3b4
Phan Thanh Tung <tungptkh@gmail.com>
2017-11-10 10:55:19 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({message: 'Hello world 23'})
+  res.json({message: 'Hello world 23456'})
 })
 app.use('/auth', authRoute)
 
